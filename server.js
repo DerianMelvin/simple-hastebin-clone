07d55f0f9abb0a65fce4f5e960de4e9ef29606ea
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-09-06 17:58:24 +0800

    EJS setup & file render - Create codeDisplay.ejs

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Test");
+  const code = `Welcome to WasteBin!
+  
+Use the commands in the top right corner
+to create a new file to share with others.`;
+
+  res.render("codeDisplay", { code });
 });
 
 app.listen(3000);
