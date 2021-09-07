85a7aae7ac6e56b61ba5801f547203951f1062a0
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-09-07 13:42:02 +0800

    Add database functionality - Create Document.js - Import mongoose - Import /models/Document.js to server.js

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+const Document = require("./models/Document");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/wastebin");
 
 app.get("/", (req, res) => {
   const code = `Welcome to WasteBin!
@@ -13,8 +18,29 @@ to create a new file to share with others.`;
   res.render("codeDisplay", { code });
 });
 
-app.get('/new', (req, res) => {
-  res.render('new');
-})
+app.get("/new", (req, res) => {
+  res.render("new");
+});
+
+app.post("/save", async (req, res) => {
+  const value = req.body.value;
+  try {
+    const document = await Document.create({ value });
+    res.redirect(`/${document.id}`);
+  } catch (e) {
+    res.render("new", { value });
+  }
+  console.log(value);
+});
+
+app.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const document = await Document.findById(id);
+    res.render("codeDisplay", { code: document.value });
+  } catch (e) {
+    res.redirect("/");
+  }
+});
 
 app.listen(3000);
